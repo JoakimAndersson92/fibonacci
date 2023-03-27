368feb86aebfad90b01f7043635dd2189304ff5c
joakimAndersson92 <joakim.andersson920202@gmail.com>
2023-03-27 08:15:36 +0200

    Add function fibsRec

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function fibs(num){
+function fibs(num) {
     let array = []
-    if(num <= 1){
+    if (num <= 1) {
         array.push(0)
         return console.log(array)
     }
     let calc = 0
     for (let i = 0; i < num - 1; i++) {
-        if(i === 0){
+        if (i === 0) {
             array.push(0)
             array.push(1)
             calc = array[i] + array[i + 1]
             array.push(calc)
             i = 1
         }
-        else{
+        else {
             calc = array[i - 1] + array[i]
             array.push(calc)
         }
@@ -22,4 +22,18 @@ function fibs(num){
     console.log(array)
 }
 
-fibs(20)
+function fibsRec(num, array = []) {
+    if (num <= 0) {
+      return array;
+    }
+    if (array.length < 2) {
+      array.push(0, 1);
+    } else {
+      const sum = array[array.length - 1] + array[array.length - 2];
+      array.push(sum);
+    }
+    return fibs(num - 1, array);
+  }
+  
+
+fibsRec(8)
